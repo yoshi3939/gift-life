1a0871aac4d53ac6dcc6b269ba850ba3cbd1486f
yoshi3939 <irieyoshimi43@gmail.com>
2021-08-04 12:27:33 +0900

    ギフト記録機能の実装

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,5 +1,25 @@
 class GiftsController < ApplicationController
+  
   def index
-    @gifts = Gift.all
+    @gifts = Gift.order("created_at DESC")
   end
+
+  def new
+    @gift = Gift.new
+  end
+
+  def create
+    @gift = Gift.create(gift_params)
+    if @gift.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def gift_params
+    params.require(:gift).permit(:status_id, :name, :image, :partner, :event_date, :event, :memo)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "gifts#index"
-  resources :gifts, only: :index
+  resources :gifts, only: [:index, :new, :create]
 end
