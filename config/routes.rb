1b6f655f08c07af34c4d88d5f4f09c5681a88ea5
yoshi3939 <irieyoshimi43@gmail.com>
2021-08-05 18:19:00 +0900

    お気に入りページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "items#index"
   resources :gifts, only: [:index, :new, :create]
   resources :items, only: :index
+  resources :interests
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_051550) do
+ActiveRecord::Schema.define(version: 2021_08_04_074008) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,16 @@ ActiveRecord::Schema.define(version: 2021_08_04_051550) do
     t.index ["user_id"], name: "index_gifts_on_user_id"
   end
 
+  create_table "interests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price"
+    t.text "memo"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_interests_on_user_id"
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.integer "price"
@@ -74,5 +84,6 @@ ActiveRecord::Schema.define(version: 2021_08_04_051550) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "gifts", "users"
+  add_foreign_key "interests", "users"
   add_foreign_key "items", "users"
 end
