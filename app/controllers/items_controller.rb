bab6b58ed6618333cd8c8d28521c33ea63c5ef92
yoshi3939 <irieyoshimi43@gmail.com>
2021-08-07 18:43:45 +0900

    商品管理機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,59 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :search]
+  before_action :if_not_admin, except: [:index, :search]
+  before_action :set_item, only: [:edit, :update, :destroy]
 
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to items_path
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_path
+  end
+
+  def search
+    @items = Item.search(params[:keyword]).order("created_at DESC")
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :price, :memo, :shop, :shopinfo).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def if_not_admin
+    unless current_user.admin?
+      redirect_to root_path
+    end
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+
+  validates :name, presence: true
+
+  def self.search(search)
+    if search != ""
+      Item.where('name LIKE(?) OR memo LIKE(?) OR shop LIKE(?) OR shopinfo LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+    else
+      Item.all
+    end
+  end
+
 end
